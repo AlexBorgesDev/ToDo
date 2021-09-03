dd9b7408b69f902d515ee1dd96b810f7da2900cb
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-09-03 14:20:19 -0300

    HandleSignIn function created

--- src/pages/SingIn/index.tsx
+++ src/pages/SingIn/index.tsx
@@ -1,25 +1,48 @@
 import { Link } from 'react-router-dom'
 import { FaCheck } from 'react-icons/fa'
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 
 import styles from './styles.module.scss'
 
 import Input from '../../components/Input'
 import SubmitButton from '../../components/SubmitButton'
 
+import signInValidation from '../../validations/singIn.validation'
+
 const SignIn = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  const [errors, setErrors] = useState<(string | undefined)[]>([])
+  const [loading, setLoading] = useState(false)
+
+  const handleSignIn = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    if (loading) return
+    setLoading(true)
+
+    const errors = await signInValidation({ email, password })
+
+    if (errors) {
+      setErrors(errors)
+      return setLoading(false)
+    } else setErrors([])
+
+    setLoading(false)
+  }
+
   return (
     <div className={styles.container}>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSignIn}>
         <h1 className={styles.title}>
           <FaCheck /> ToDo
         </h1>
 
         <Input
           value={email}
+          error={errors.includes('email')}
+          disabled={loading}
           placeholder="Email"
           containerClass={styles.input}
           onChange={event => setEmail(event.target.value)}
@@ -28,11 +51,14 @@ const SignIn = () => {
         <Input
           type="password"
           value={password}
+          error={errors.includes('password')}
+          disabled={loading}
+          maxLength={16}
           placeholder="Password"
           onChange={event => setPassword(event.target.value)}
         />
 
-        <SubmitButton>SIGN IN</SubmitButton>
+        <SubmitButton loading={loading}>SIGN IN</SubmitButton>
 
         <span className={styles.or}>OU</span>
 
