1eb13a5d1e1d9ad5d123150757d351aaf6b56dcc
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-09-03 17:19:33 -0300

    Created the ToDo component

---
+++ src/components/ToDo/index.tsx
@@ -0,0 +1,48 @@
+import { memo, useState } from 'react'
+import { FaCheck, FaTrash } from 'react-icons/fa'
+
+import styles from './styles.module.scss'
+
+export interface ToDoProps {
+  id: string
+  task: string
+  index?: number
+  completed: boolean
+  createdAt: number
+  updatedAt?: number
+  onRemove: (props: { id: string; index?: number }) => void
+}
+
+const ToDo = (props: ToDoProps) => {
+  const [completed, setCompleted] = useState(props.completed)
+
+  const handleCheck = () => {
+    setCompleted(!completed)
+  }
+
+  const onRemove = () => props.onRemove({ id: props.id, index: props.index })
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.checkBox}>
+        <button
+          type="button"
+          className={completed ? styles.check : undefined}
+          onClick={handleCheck}
+        >
+          {completed && <FaCheck />}
+        </button>
+      </div>
+
+      <p className={styles.content}>{props.task}</p>
+
+      <div className={styles.trash}>
+        <button type="button" onClick={onRemove}>
+          <FaTrash />
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default memo(ToDo)
