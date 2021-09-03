d324a245d375e22e7815b8e1f6de73244b84876d
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-09-03 15:01:18 -0300

    HandleSignUp function created

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,26 +1,47 @@
-import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import { FaCheck } from 'react-icons/fa'
+import { FormEvent, useState } from 'react'
 
 import styles from './styles.module.scss'
 
 import Input from '../../components/Input'
 import SubmitButton from '../../components/SubmitButton'
-import { Link } from 'react-router-dom'
+
+import singUpValidation from '../../validations/singUp.validation'
 
 const SignUp = () => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  const [errors, setErrors] = useState<(string | undefined)[]>([])
+  const [loading, setLoading] = useState(false)
+
+  const handleSignUp = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    setLoading(true)
+
+    const errors = await singUpValidation({ email, name, password })
+
+    if (errors) {
+      setErrors(errors)
+      return setLoading(false)
+    } else setErrors([])
+
+    setLoading(false)
+  }
+
   return (
     <div className={styles.container}>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSignUp}>
         <h1 className={styles.title}>
           <FaCheck /> ToDo
         </h1>
 
         <Input
           value={name}
+          error={errors.includes('name')}
+          disabled={loading}
           placeholder="Name"
           containerClass={styles.input}
           onChange={event => setName(event.target.value)}
@@ -28,6 +49,8 @@ const SignUp = () => {
 
         <Input
           value={email}
+          error={errors.includes('email')}
+          disabled={loading}
           placeholder="Email"
           containerClass={styles.input}
           onChange={event => setEmail(event.target.value)}
@@ -36,11 +59,15 @@ const SignUp = () => {
         <Input
           type="password"
           value={password}
+          error={errors.includes('password')}
+          disabled={loading}
           placeholder="Password"
           onChange={event => setPassword(event.target.value)}
         />
 
-        <SubmitButton>SIGN UP</SubmitButton>
+        <SubmitButton loading={loading} disabled={loading}>
+          SIGN UP
+        </SubmitButton>
 
         <span className={styles.or}>OU</span>
 
