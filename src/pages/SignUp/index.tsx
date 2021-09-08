091e4ba79d4033206ca2ce90da29d7cffbdc161f
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-09-08 14:43:33 -0300

    Changed handleSignUp function

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -7,7 +7,7 @@ import styles from './styles.module.scss'
 import Input from '../../components/Input'
 import SubmitButton from '../../components/SubmitButton'
 
-import singUpValidation from '../../validations/singUp.validation'
+import signUpService, { SSignUpProps } from '../../services/signUp.service'
 
 const SignUp = () => {
   const [name, setName] = useState('')
@@ -17,16 +17,29 @@ const SignUp = () => {
   const [errors, setErrors] = useState<(string | undefined)[]>([])
   const [loading, setLoading] = useState(false)
 
+  const handleError: SSignUpProps['onError'] = async err => {
+    if (err.response.status === 400) {
+      setErrors(err.response.data.keys || [])
+    }
+  }
+
   const handleSignUp = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     setLoading(true)
 
-    const errors = await singUpValidation({ email, name, password })
+    const response = await signUpService({
+      data: { email, name, password },
+      onError: handleError,
+      onDataError: setErrors,
+    })
 
-    if (errors) {
-      setErrors(errors)
-      return setLoading(false)
-    } else setErrors([])
+    if (response) {
+      alert(response.message)
+
+      setName('')
+      setEmail('')
+      setPassword('')
+    }
 
     setLoading(false)
   }
