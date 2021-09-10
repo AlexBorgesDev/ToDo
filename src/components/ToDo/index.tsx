dee8342f2d774b11d67a6464f69ff84844b8cbae
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-09-10 12:28:39 -0300

    Completed to ToDo component functionalities

--- src/components/ToDo/index.tsx
+++ src/components/ToDo/index.tsx
@@ -1,7 +1,10 @@
+import { useHistory } from 'react-router-dom'
 import { memo, useState } from 'react'
 import { FaCheck, FaTrash } from 'react-icons/fa'
 
 import styles from './styles.module.scss'
+
+import updateToDoService from '../../services/toDos/updateToDo.service'
 
 export interface ToDoProps {
   id: string
@@ -14,10 +17,35 @@ export interface ToDoProps {
 }
 
 const ToDo = (props: ToDoProps) => {
+  const history = useHistory()
+
   const [completed, setCompleted] = useState(props.completed)
 
-  const handleCheck = () => {
-    setCompleted(!completed)
+  const handleCheck = async () => {
+    const newValue = !completed
+    setCompleted(newValue)
+
+    const token = localStorage.getItem('@session_token')
+    const refreshToken = localStorage.getItem('@session_refresh_token')
+
+    if (!token || !refreshToken) return handleLogOut()
+
+    try {
+      await updateToDoService({
+        id: props.id,
+        completed: newValue,
+        token,
+        refreshToken,
+      })
+    } catch (err) {
+      setCompleted(!newValue)
+      alert('Something went wrong while trying to update a ToDo')
+    }
+  }
+
+  const handleLogOut = () => {
+    localStorage.clear()
+    history.push('/')
   }
 
   const onRemove = () => props.onRemove({ id: props.id, index: props.index })
